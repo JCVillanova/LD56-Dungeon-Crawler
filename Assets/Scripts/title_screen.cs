da1187244914f3f286e83da322e0fa4d9a25047a
JoshuaCVillanova <joshuacvillanova@gmail.com>
2024-10-06 15:43:30 -0700

    Create title screen scene
    Add a title screen with a game title as well as play, credits, and quit buttons. Play button loads the 'Game' scene with the mouse. Quit button exits the game (when it is a build, not in the editor). Credits button is not yet functional

---
+++ Assets/Scripts/title_screen.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class title_screen : MonoBehaviour
+{
+    public string gameScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play() 
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
