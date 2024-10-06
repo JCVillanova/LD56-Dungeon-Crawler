bdb0dd8a6e1e486df6d990cc82afde042aa795df
BrianFellDownTheWell <123607721+BrianFellDownTheWell@users.noreply.github.com>
2024-10-05 18:15:01 -0700

    Added textures to cockroaches

--- Assets/Scripts/Cockroach.cs
+++ Assets/Scripts/Cockroach.cs
@@ -7,16 +7,23 @@ public class Cockroach : MonoBehaviour
     [SerializeField] private float xSpeed;
     [SerializeField] private float ySpeed;
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
     private Vector2 velocity;
     [SerializeField] public int direction; // The direction; 0 is north, 1 is east, 2 is south, 3 is west (the cockroach shall move in a square)
     [SerializeField] private float intervalBeforeDirectionChange;
     public float timeSinceChange;
+
+    [SerializeField] private Sprite north;
+    [SerializeField] private Sprite east;
+    [SerializeField] private Sprite south;
+    [SerializeField] private Sprite west;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         timeSinceChange = Time.time;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -33,18 +40,22 @@ public class Cockroach : MonoBehaviour
             if (direction == 0)
             {
                 rb.velocity = new Vector2(0, ySpeed);
+                spriteRenderer.sprite = north;
             }
             if (direction == 1)
             {
                 rb.velocity = new Vector2(xSpeed, 0);
+                spriteRenderer.sprite = east;
             }
             if (direction == 2)
             {
                 rb.velocity = new Vector2(0, -1.0f * ySpeed);
+                spriteRenderer.sprite = south;
             }
             if (direction == 3)
             {
                 rb.velocity = new Vector2(-1.0f * xSpeed, 0);
+                spriteRenderer.sprite = west;
             }
             //dist--;
         }
